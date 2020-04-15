753a90bca538a883346cbfdc11873a7dd52e97c9
Reiker Seiffe <reikerseiffe@gmail.com>
2020-04-15 00:35:17 -0500

    Added groupName functionality and cleaned up format

---
+++ queries/CalculateInputsFromTypeName.sql
@@ -0,0 +1,8 @@
+SELECT industryActivityMaterials.typeID, industryActivityMaterials.activityID, industryActivityMaterials.materialTypeID, industryActivityMaterials.quantity, invTypes.typeName, invTypes.groupID, invGroups.groupName
+FROM EVE_SDE.industryActivityMaterials 
+join invTypes on industryActivityMaterials.materialTypeID = invTypes.typeID join invGroups on invTypes.groupID = invGroups.groupID
+where industryActivityMaterials.typeID = (
+	SELECT industryActivityProducts.typeID FROM EVE_SDE.industryActivityProducts where productTypeID = (
+		SELECT  typeID FROM EVE_SDE.invTypes where typeName = ?
+    )
+) and industryActivityMaterials.activityID = 1 order by activityID;
--- queries/CalculateReactionInputsFromTypeName.sql
+++
@@ -1,7 +0,0 @@
-SELECT industryActivityMaterials.typeID, industryActivityMaterials.activityID, industryActivityMaterials.materialTypeID, industryActivityMaterials.quantity, invTypes.typeName, invTypes.groupID FROM EVE_SDE.industryActivityMaterials 
-inner join invTypes on industryActivityMaterials.materialTypeID = invTypes.typeID
-where industryActivityMaterials.typeID = (
-	SELECT industryActivityProducts.typeID FROM EVE_SDE.industryActivityProducts where productTypeID = (
-		SELECT  typeID FROM EVE_SDE.invTypes where typeName = ?
-    )
-) and industryActivityMaterials.activityID = 1 order by activityID;
